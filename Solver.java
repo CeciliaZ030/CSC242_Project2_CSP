ef99b0f49905e8313cc8713b9f17d114c084a8e1
CeciliaZ030 <45245961+CeciliaZ030@users.noreply.github.com>
2019-02-24 22:33:13 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -8,90 +8,52 @@ public class Solver {
 		this.CSP_instantce = instance;
 	}
 	
-	AustraliaCSP aus_Solver(AustraliaCSP aus_CSP) {
-
-		
-		/**
-		 * To do
-		 * */
-		
-		return aus_CSP;
-	}
 	
-	
-	LinkedList<City> aus_Backtrack_Search(AustraliaCSP aus_CSP) {
-		LinkedList<City> assignment =  new LinkedList<City>();
-		return Backtrack(assignment, aus_CSP);	
-	}
-	
-	LinkedList<City> Backtrack(LinkedList<City> assignment, AustraliaCSP aus_CSP) {
+	LinkedList<City> aus_Solver(AustraliaCSP aus_CSP) {
+		LinkedList<City> Assignment =  new LinkedList<City>();
 		LinkedList<City> shadow_copy = (LinkedList<City>) aus_CSP.Cities.clone();
-		if (assignment.size() == aus_CSP.var_num) {
-			return assignment;
+		return Backtrack(Assignment, aus_CSP, shadow_copy);
+	}
+	
+	LinkedList<City> Backtrack(LinkedList<City> Assignment, AustraliaCSP aus_CSP, LinkedList<City> shadow_copy) {
+		if (Assignment.size() == aus_CSP.var_num) {
+			return Assignment;
 		}
 		City var = shadow_copy.remove();
-		for (Character i : var.Domain) {
-			var.assignment = i;
-			if(aus_CSP.isConsistant(var, assignment)) {
-				assignment.add(var);
-				LinkedList<City> temp = aus_CSP.Propagate(assignment);
-				if(temp != null) {
-					//temp is null when the propagation fail
-					assignment.addAll(temp);
-					LinkedList<City> result = Backtrack(assignment, aus_CSP);
-					if (result != null) {
-						return result;
-					}
+		
+		while(!var.Domain.isEmpty()) {
+			var.assignment = var.Domain.remove();
+			if(aus_CSP.isConsistant(var, Assignment)) {
+				Assignment.add(var);
+				LinkedList<City> result = Backtrack(Assignment, aus_CSP, shadow_copy);
+				if (result != null) {
+					return result;
 				}
 			}
-			var.Domain.remove(i);
+			var.Domain.remove(var.assignment);
 		}
+		
 		return null;
 	}
 	
-	public boolean aus_revise(AustraliaCSP aus_CSP, City i, City j) {
-		boolean changed = false;
-		  
-		Aus_Constraint C = new Aus_Constraint(i, i);
-		  
-		for(Character vi : i.Domain) {
-			i.assignment = vi;
-			boolean ok = false;
-			for(Character vj : j.Domain) {
-				j.assignment = vj;
-				if(C.check()) {
-					ok = true;
-				}
-			}
-			if(!ok) {
-				i.Domain.remove(vi);
-				changed = true;
-			}
+	
+	void print(LinkedList<City> result) {
+		for (City c : result) {
+			System.out.println("shadow copy: " +c.name);
 		}
-		  
-		return changed;
+		System.out.println();
 	}
+	
+	void print2(LinkedList<City> result) {
+		System.out.println("Assigned");
 
-	public boolean aus_AC3(AustraliaCSP aus_CSP) {
-		 LinkedList<Aus_Constraint> newList = (LinkedList<Aus_Constraint>) aus_CSP.Constraint.clone();
-		 
-		 while(!newList.isEmpty()) {
-			 Aus_Constraint ijSet = newList.poll();
-			 City i = ijSet.c1;
-			 City j = ijSet.c2;
-			 if(aus_revise(aus_CSP, i, j)) {
-				 if(i.Domain.isEmpty()) {
-					 return false;
-				 }
-				 for(City k : i.neighbors) {
-					 newList.add(new Aus_Constraint(k, i));
-				 }
-			 }
-		  }
-		  aus_CSP.Constraint = (LinkedList<Aus_Constraint>) newList;
-		  return true;
+		for (City c : result) {
+			System.out.println(c.name + ": " +c.assignment);
+		}
+		System.out.println();
 	}
-
+	
+	
 	
 	NQueenCSP NQ_Solver(NQueenCSP nQueen_CSP) {
 		
@@ -102,6 +64,8 @@ public class Solver {
 		
 		return nQueen_CSP;
 	}
+	
+	
 	
 	SchedulingCSP schedul_Solver(SchedulingCSP schedul_CSP) {
 		
